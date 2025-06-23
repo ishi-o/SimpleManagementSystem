1db3157c140a703a2e4a38233f6a066b02bade67
ishi-o <458457289@qq.com>
2025-06-23 17:01:50 +0800

    init: 初始化项目
    本项目是一个Spring Boot Maven项目

---
+++ src/main/java/com/manasys/manasys/ManasysApplication.java
@@ -0,0 +1,13 @@
+package com.manasys.manasys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManasysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManasysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manasys/manasys/ManasysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manasys.manasys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManasysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
